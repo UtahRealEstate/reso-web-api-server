8fece73ac72cc6c58e6da6ff0ec1590c5cd473e6
Fred Larsen <fredish@gmail.com>
2014-04-17 02:43:08 -0600

    config

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    sslPort         : 9999,
+    port            : 9990,
+    tokenExpire     : 3600,
+    authCodeExpire  : 600,
+    hostname        : 'vv.fredish.com',
+    redirectURI     : 'http://vv.fredish.com:8080/auth/appexample/callback',
+    sslKey          : './certs/sample.key',
+    sslCert         : './certs/sample.crt'
+}
---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    sslPort         : 443,
+    port            : 80,
+    tokenExpire     : 3600,
+    authCodeExpire  : 600,
+    hostname        : 'reso.utahrealestate.com',
+    redirectURI     : 'http://reso.wfrmls.com:8080/auth/appexample/callback',
+    sslKey          : './certs/utahrealestate.com.key',
+    sslCert         : './certs/utahrealestate.com.crt'
+}
